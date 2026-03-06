d94e70ad4dde7b2812f1c7cb9e6cc8c54e0db2c3
jujasa0-arch <jujasa0@gmail.com>
2026-03-06 16:07:24 +0300

    initial clean commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 'success',
+        message: `Hello Kariuki ${PORT}`,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
